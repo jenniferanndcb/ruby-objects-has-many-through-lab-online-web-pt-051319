876f08c387e7e6ab5aa802ed040a70a97d48c389
Jen <jenniferanndcb@gmail.com>
2019-06-13 14:30:31 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor 
+  
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(patient, date)
+    appt1 = Appointment.new(patient)
+  end 
+  
+  def appointments 
+    Appointment.all.select {|doctor| doctor.name
+  end 
+  
+end 
